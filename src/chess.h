f4cfaa75bab9f9a47221064a6bd27374545035fe
yelmokht <younes.el.mokhtari@ulb.be>
2023-11-06 17:58:09 +0100

    Update chess.h

--- src/chess.h
+++ src/chess.h
@@ -3,10 +3,12 @@
 
 typedef struct {
   //TODO
+  //One attribute for each line and a list of string for the movements
 } chessgame;
 
 typedef struct {
   //TODO
+  //char* string FEN or check SCL_boardToFEN
 } chessboard;
 
 #endif
