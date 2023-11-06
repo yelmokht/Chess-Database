4da6c509c34668f51dba103af5d8772e369ea8b4
yelmokht <younes.el.mokhtari@ulb.be>
2023-11-06 16:03:58 +0100

    Setup chess.c and chess.h

--- src/chess.h
+++ src/chess.h
@@ -1,32 +1,12 @@
-/*
- * chess.h
- * TODO
- */
+#ifndef _CHESS_H
+#define _CHESS_H
 
-/* Structure to represent complex numbers */
-typedef struct
-{
-  double    a,
-            b;
-} Complex;
+typedef struct {
+  //TODO
+} chessgame;
 
-/* fmgr macros complex type */
+typedef struct {
+  //TODO
+} chessboard;
 
-#define DatumGetComplexP(X)  ((Complex *) DatumGetPointer(X))
-#define ComplexPGetDatum(X)  PointerGetDatum(X)
-#define PG_GETARG_COMPLEX_P(n) DatumGetComplexP(PG_GETARG_DATUM(n))
-#define PG_RETURN_COMPLEX_P(x) return ComplexPGetDatum(x)
-
-/*****************************************************************************/
-
-#define EPSILON         1.0E-06
-
-#define FPzero(A)       (fabs(A) <= EPSILON)
-#define FPeq(A,B)       (fabs((A) - (B)) <= EPSILON)
-#define FPne(A,B)       (fabs((A) - (B)) > EPSILON)
-#define FPlt(A,B)       ((B) - (A) > EPSILON)
-#define FPle(A,B)       ((A) - (B) <= EPSILON)
-#define FPgt(A,B)       ((A) - (B) > EPSILON)
-#define FPge(A,B)       ((B) - (A) <= EPSILON)
-
-/*****************************************************************************/
+#endif
