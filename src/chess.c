fc416912c013795a589028e675bf7eb68895b182
yelmokht <younes.el.mokhtari@ulb.be>
2023-11-25 18:12:26 +0100

    First implementation of functions

--- src/chess.c
+++ src/chess.c
@@ -10,23 +10,40 @@
 #include <float.h>
 #include <math.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "utils/builtins.h"
 #include "libpq/pqformat.h"
+#include "smallchesslib.h"
 #include "chess.h"
 
 PG_MODULE_MAGIC;
 
+/*****************************************************************************/
+
+/* Constructors */
+
+/**
+ * @brief Constructs a chessgame
+ * @param pgn PGN notation of the chessgame
+ * @return *chessgame_t Pointer to the chessgame
+ */
+static chessgame_t *chessgame_make(const char *pgn) {
+  chessgame_t *chessgame = palloc0(sizeof(chessgame_t));
+  strcpy(chessgame->pgn, pgn);
+  return chessgame;
+}
+
+
 /**
  * @brief Constructs a chessboard
  * @param piece_placement_data Piece placement data
- * @param active_color
- * @param castling_availability
- * @param en_passant_target_square
- * @param halfmove_clock
- * @param fullmove_clock
+ * @param active_color Active color
+ * @param castling_availability Castling availability
+ * @param en_passant_target_square En passant target square
+ * @param halfmove_clock Halfmove clock
+ * @param fullmove_clock Fullmove clock
+ * @return *chessboard_t Pointer to the chessboard
  */
-static chessboard_t * chessboard_make(char *piece_placement_data, char active_color, char *castling_availability, char *en_passant_target_square, int halfmove_clock, int fullmove_clock) {
+static chessboard_t *chessboard_make(char *piece_placement_data, char active_color, char *castling_availability, char *en_passant_target_square, int halfmove_clock, int fullmove_clock) {
   chessboard_t *chessboard = (chessboard_t *)malloc(sizeof(chessboard_t));
   strcpy(chessboard->piece_placement_data, piece_placement_data);
   chessboard->active_color = active_color;
@@ -37,7 +54,29 @@ static chessboard_t * chessboard_make(char *piece_placement_data, char active_co
   return chessboard;
 }
 
-static chessboard_t * FEN_parsing(char* fen) {
+/*****************************************************************************/
+
+/* Functions for data types */
+
+/**
+  * @brief Converts PGN to a chessgame_t
+  * @param pgn PGN notation
+  * @return *chessgame_t Pointer to the chessgame
+  */
+static chessgame_t *
+PGN_to_chessgame(char *pgn)
+{
+  return chessgame_make(pgn);
+}
+
+/**
+  * @brief Converts FEN to a chessboard_t
+  * @param fen FEN notation
+  * @return *chessboard_t Pointer to the chessboard
+  */
+static chessboard_t *
+FEN_to_chessboard(char* fen) 
+{
   char *token = strtok(fen, " ");
   char *list[6];
   int index = 0;
@@ -51,37 +90,98 @@ static chessboard_t * FEN_parsing(char* fen) {
   return chessboard_make(list[0], *list[1], list[2], list[3], atoi(list[4]), atoi(list[5]));
 }
 
-void print_chessboard(chessboard_t *chessboard) {
-  printf("Piece placement data : %s", chessboard->piece_placement_data);
-  printf("Active color : %d", chessboard->active_color);
-  printf("Castling availability : %s", chessboard->castling_availability);
-  printf("En passant target square : %s", chessboard->en_passant_target_square);
-  printf("Halfmove clock : %d", chessboard->halfmove_clock);
-  printf("Fullmove clock: %d", chessboard->fullmove_clock);
-} 
- 
+/*****************************************************************************/
+
+/* Functions */
+
+/**
+  * @brief Returns the board state at a given half-move.
+  * @param chessgame Pointer to the chessgame
+  * @param number_half_moves Number of half-moves
+  * @return *chessboard_t Pointer to the chessboard
+  */
 PG_FUNCTION_INFO_V1(getBoard);
-
-Datum getBoard(PG_FUNCTION_ARGS) {
-  //TODO
+Datum
+getBoard(PG_FUNCTION_ARGS)
+{
+  chessgame_t *chessgame = PG_GETARG_CHESSGAME_P(0);
+  uint16_t number_half_moves = PG_GETARG_INT16(1);
+  SCL_Record record;
+  SCL_recordFromPGN(record, chessgame->pgn);
+  SCL_Board board;
+  SCL_recordApply(record, board, number_half_moves);
+  char *fen;
+  SCL_boardToFEN(board, fen); //vérifer boolean
+  chessboard_t *chessboard = FEN_to_chessboard(fen);
+  PG_RETURN_CHESSBOARD_P(chessboard); //Don"t forget to free SCL structures
 }
 
+/**
+  * @brief Returns the chessgame truncated to its first N half-moves.
+  * @param chessgame Pointer to the chessgame
+  * @param number_half_moves Number of half-moves
+  * @return *chessgame_t Pointer to the chessgame
+  */
 PG_FUNCTION_INFO_V1(getFirstMoves);
+Datum 
+getFirstMoves(PG_FUNCTION_ARGS) 
+{
+  // Two possibilities : One with SCL_recordRemoveLast(SCL_Record r) and one with truncate function that we have to implement here
+  // 1.
+  // chessgame -> SCL_Record
+  // calcule le nombre de moves à enlever => length - integer
+  // for loop avec SCL_recordRemoveLast(SCL_Record r) -> SCL_Record
+  // SCL_Record -> chessgame
+  // 2.
+  // chessgame truncate to n first moves -> new chessgame
 
-Datum getFirstMoves(PG_FUNCTION_ARGS) {
-  //TODO
+  chessgame_t *chessgame = PG_GETARG_CHESSGAME_P(0);
+  uint16_t number_half_moves = PG_GETARG_INT16(1);
+  SCL_Record record;
+  SCL_recordFromPGN(record, chessgame->pgn);
+  uint16_t length = SCL_recordLength(record);
+  uint16_t number_moves_to_remove = length - number_half_moves; //vérifier que length > number_half_moves
+  for (int i = 0; i < number_moves_to_remove; i++) {
+    SCL_recordRemoveLast(record);
+  }
+  SCL_PutCharFunction putCharFunc;
+  SCL_Board initialState = SCL_boardFromFEN(STARTING_POSITION);
+  SCL_printPGN(record, putCharFunc, initialState);
+  chessgame_t *new_chessgame = PGN_to_chessgame(putCharFunc); // new chessgame or chessgame ?
+  PG_RETURN_CHESSGAME_P(new_chessgame)
 }
 
+/**
+  * @brief Returns true if the first chess game starts with the exact same set of moves as the second chess game.
+  * The second parameter should not be a full game, but should only contain the opening moves that we want to check for, which can be of any length, i.e., m half-moves.
+  * @param chessgame Pointer to the first chessgame
+  * @param chessgame Pointer to the second chessgame
+  * @return bool True if the first chessgame starts with the exact same set of moves as the second chessgame
+  */
 PG_FUNCTION_INFO_V1(hasOpening);
-
-Datum hasOpening(PG_FUNCTION_ARGS) {
-  //TODO
-  PG_RETURN_BOOL(false);
+Datum
+hasOpening(PG_FUNCTION_ARGS)
+{
+  chessgame_t *chessgame_1 = PG_GETARG_CHESSGAME_P(0);
+  chessgame_t *chessgame_2 = PG_GETARG_CHESSGAME_P(1);
+  PG_RETURN_BOOL(strcmp(chessgame_1->pgn, chessgame_2->pgn) == 0);
 }
 
+/**
+  * @brief  Returns true if the chessgame contains the given board state in its first N half-moves.
+  * Only compare the state of the pieces and not compare the move count, castling right, en passant pieces, ..
+  * @param chessgame Pointer to the chessgame
+  * @param chessboard Pointer to the chessboard
+  * @param number_half_moves Number of half-moves
+  * @return bool True if the chessgame contains the given board state in its first N half-moves
+*/
 PG_FUNCTION_INFO_V1(hasBoard);
-
-Datum hasBoard(PG_FUNCTION_ARGS) {
-  //TODO
-  PG_RETURN_BOOL(false);
+Datum
+hasBoard(PG_FUNCTION_ARGS)
+{
+  chessgame_t *chessgame = PG_GETARG_CHESSGAME_P(0);
+  chessboard_t *chessboard = PG_GETARG_CHESSBOARD_P(1);
+  uint16_t number_half_moves = PG_GETARG_INT16(2);
+  chessboard_t *chessboard_of_chessgame = getBoard(chessgame, number_half_moves);
+  PG_RETURN_BOOL(strcmp(chessboard_of_chessgame->piece_placement_data, chessboard->piece_placement_data) == 0);
 }
--- src/chess.h
+++ src/chess.h
@@ -10,6 +10,18 @@
 #define STARTING_HALFMOVE_CLOCK 0
 #define STARTING_FULLMOVE_CLOCK 1
 
+/**
+ * @brief Structure containing all information representing a chess game state using SAN notation.
+ */
+typedef struct {
+  const char* pgn;
+} chessgame_t;
+
+/* fmgr macros chessgame type */
+#define DatumGetChessgameP(X)  ((chessgame_t *) DatumGetPointer(X))
+#define ChessgamePGetDatum(X)  PointerGetDatum(X)
+#define PG_GETARG_CHESSGAME_P(n) DatumGetChessgameP(PG_GETARG_DATUM(n))
+#define PG_RETURN_CHESSGAME_P(x) return ChessgamePGetDatum(x)
 
 /**
  * @brief Structure containing all information representing a chess board state using FEN notation.
@@ -23,8 +35,16 @@ typedef struct {
   int fullmove_clock;
 } chessboard_t;
 
+/* fmgr macros chessboard type */
+#define DatumGetChessboardP(X)  ((chessboard_t *) DatumGetPointer(X))
+#define ChessboardPGetDatum(X)  PointerGetDatum(X)
+#define PG_GETARG_CHESSBOARD_P(n) DatumGetChessboardP(PG_GETARG_DATUM(n))
+#define PG_RETURN_CHESSBOARD_P(x) return ChessboardPGetDatum(x)
+
+static chessgame_t *chessgame_make(char *san);
 static chessboard_t *chessboard_make(char *piece_placement_data, char active_color, char *castling_availability, char *en_passant_target_square, int halfmove_clock, int fullmove_clock);
-chessboard_t * FEN_parsing(char* fen);
-void print_chessboard(chessboard_t *chessboard);
+
+static chessgame_t *PGN_to_chessgame(char *pgn) 
+static chessboard_t *FEN_to_chessboard(char* fen);
 
 #endif
