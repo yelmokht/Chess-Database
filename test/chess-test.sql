4936a6cc0ff184736742a2e395ae1dd62c4af92c
yelmokht <younes.el.mokhtari@ulb.be>
2023-11-28 14:34:24 +0100

    Add test script and chess-test.sql

--- test/chess-test.sql
+++ test/chess-test.sql
@@ -1,31 +1,12 @@
-CREATE EXTENSION complex;
+CREATE TABLE games (game chessgame);
 
-CREATE TABLE t (id integer, z complex);
+CREATE TABLE favoriteGames (game chessgame);
 
-INSERT INTO t VALUES
-(1, '(1, 0)'),
-(2, '(0, 1)'),
-(3, '(-1, 0)'),
-(4, '(0, -1)');
+INSERT INTO games VALUES
+('1.e4 e6 2.d4 d5 3.Nd2 Nf6 4.e5 Nfd7 5.f4 c5 6.c3 Nc6 7.Ndf3 cxd4 '),
+('1.e4 e6 2.d4 d5 3.Nd2 c5 4.exd5 Qxd5 5.Ngf3 cxd4 6.Bc4 Qd6 7.O-O Nf6 '),
+('1.e4 c6 2.c4 d5 3.exd5 cxd5 4.cxd5 Nf6 5.Nc3 g6 6.Bc4 Bg7 7.Nf3 O-O '),
+('1.e4 c6 2.c4 d5 3.exd5 cxd5 4.cxd5 Nf6 5.Nc3 Nxd5 6.d4 Nc6 7.Nf3 e6 '),
+('1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nf6 5.Nc3 a6 6.Bg5 e6 7.f4 Be7 ');
 
-SELECT * FROM t;
-
-SELECT z as z, 
-    re(z) as "Re(z)", 
-    im(z) as "Im(z)"
---  conjugate(z) as "z*"
-FROM t;
-
-SELECT * FROM t WHERE z << '(0, 0)';
--- SELECT * FROM t WHERE z >> '(0, 0)';
-SELECT * FROM t WHERE z <<| '(0, 0)';
--- SELECT * FROM t WHERE z |>> '(0, 0)';
-
-SELECT a.z as a, 
-    b.z as b, 
-    a.z + b.z as "a + b", 
---  a.z - b.z as "a - b",
---  a.z * b.z as "a*b", 
-    a.z / b.z as "a / b",
-    a.z <-> b.z as "a <-> b"
-FROM t a, t b;
+SELECT * FROM games;
